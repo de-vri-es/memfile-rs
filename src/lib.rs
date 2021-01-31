59a9d1152c3768c48a69316e5ce585c0c119721b
Maarten de Vries <maarten@de-vri.es>
2021-02-01 00:33:34 +0100

    Initial commit.

---
+++ src/lib.rs
@@ -0,0 +1,243 @@
+use filedesc::FileDesc;
+use enumflags2::BitFlags;
+use std::os::unix::io::{AsRawFd, FromRawFd, IntoRawFd, RawFd};
+
+mod sys;
+
+/// A memory backed file that can have seals applied to it.
+///
+/// The struct implements [`AsRawFd`], [`IntoRawFd`] and [`FromRawFd`].
+/// When using [`FromRawFd::from_raw_fd`], you must ensure that the file descriptor is a valid `memfd`.
+pub struct MemFd {
+	fd: filedesc::FileDesc,
+}
+
+impl MemFd {
+	/// Create a new `memfd` with the given options.
+	///
+	/// The close-on-exec flag is set on the created file descriptor.
+	/// If you want to pass it to a child process, you should use [`libc::dup2`] or something similar *after forking*.
+	/// Disabling the close-on-exec flag before forking causes a race condition with other threads.
+	pub fn create(name: &str, options: &CreateOptions) -> std::io::Result<Self> {
+		let fd = sys::memfd_create(name, options.as_flags())?;
+		Ok(Self { fd })
+	}
+
+	/// Wrap an already-open file as MemFd.
+	///
+	/// This function returns an error if the fd is not a `memfd`.
+	///
+	/// If the function succeeds, the passed in file object is consumed and the returned [`MemFd`] takes ownership of the file descriptor.
+	/// If the function fails, the original file object is included in the returned error.
+	pub fn from_fd<T: AsRawFd + IntoRawFd>(fd: T) -> Result<Self, FromFdError<T>> {
+		match sys::memfd_get_seals(fd.as_raw_fd()) {
+			Ok(_) => Ok(Self { fd: FileDesc::new(fd) }),
+			Err(error) => Err(FromFdError { error, fd }),
+		}
+	}
+
+	/// Get the active seals of the file.
+	pub fn get_seals(&self) -> std::io::Result<Seals> {
+		let seals = sys::memfd_get_seals(self.as_raw_fd())?;
+		Ok(Seals::from_bits_truncate(seals as u32))
+	}
+
+	/// Add a single seal to the file.
+	///
+	/// If you want to add multiple seals, you should prefer [`Self::add_seals`] to reduce the number of syscalls.
+	///
+	/// This function will fail if the file was not created with sealing support,
+	/// if the file has already been sealed with [`Seal::Seal`],
+	/// or if you try to add [`Seal::Write`] while a shared, writable memory mapping exists for the file.
+	///
+	/// Adding a seal that is already active is a no-op.
+	pub fn add_seal(&self, seal: Seal) -> std::io::Result<()> {
+		self.add_seals(seal.into())
+	}
+
+	/// Add multiple seals to the file.
+	///
+	/// This function will fail if the file was not created with sealing support,
+	/// if the file has already been sealed with [`Seal::Seal`],
+	/// or if you try to add [`Seal::Write`] while a shared, writable memory mapping exists for the file.
+	///
+	/// Adding seals that are already active is a no-op.
+	pub fn add_seals(&self, seals: Seals) -> std::io::Result<()> {
+		sys::memfd_add_seals(self.as_raw_fd(), seals.bits() as std::os::raw::c_int)
+	}
+}
+
+impl FromRawFd for MemFd {
+	unsafe fn from_raw_fd(fd: RawFd) -> Self {
+		let fd = FileDesc::from_raw_fd(fd);
+		Self { fd }
+	}
+}
+
+impl AsRawFd for MemFd {
+	fn as_raw_fd(&self) -> RawFd {
+		self.fd.as_raw_fd()
+	}
+}
+
+impl IntoRawFd for MemFd {
+	fn into_raw_fd(self) -> RawFd {
+		self.fd.into_raw_fd()
+	}
+}
+
+/// A set of [seals][Seal].
+pub type Seals = enumflags2::BitFlags<Seal>;
+
+/// A seal that prevents certain actions from being performed on a file.
+///
+/// Note that seals apply to a file, not a file descriptor.
+/// If two file descriptors refer to the same file, they share the same set of seals.
+///
+/// Seals can not be removed from a file once applied.
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Ord, PartialOrd, Hash, BitFlags)]
+#[repr(u32)]
+#[non_exhaustive]
+pub enum Seal {
+	/// Prevent adding more seals to the file.
+	Seal = libc::F_SEAL_SEAL as u32,
+
+	/// Prevent the file from being shrunk with `truncate` or similar.
+	///
+	/// Combine with [`Seal::Grow`] to prevent the file from being resized in any way.
+	Shrink = libc::F_SEAL_SHRINK as u32,
+
+	/// Prevent the file from being extended with `truncate`, `fallocate` or simillar.
+	///
+	/// Combine with [`Seal::Shrink`] to prevent the file from being resized in any way.
+	Grow = libc::F_SEAL_GROW as u32,
+
+	/// Prevent write to the file.
+	///
+	/// This will block *all* writes to the file and prevents any shared, writable memory mappings from being created.
+	///
+	/// If a shared, writable memory mapping already exists, adding this seal will fail.
+	Write = libc::F_SEAL_WRITE as u32,
+
+	/// Similar to [`Seal::Write`], but allows existing shared, writable memory mappings to modify the file contents.
+	///
+	/// This can be used to share a read-only view of the file with other processes,
+	/// while still being able to modify the contents through an existing mapping.
+	FutureWrite = libc::F_SEAL_FUTURE_WRITE as u32,
+}
+
+/// Options for creating a [`MemFd`].
+///
+/// Support for options depend on platform and OS details.
+/// Refer to your kernel documentation of `memfd_create` for details.
+#[derive(Copy, Clone, Debug, Default)]
+pub struct CreateOptions {
+	allow_sealing: bool,
+	huge_table: Option<HugeTlb>,
+}
+
+impl CreateOptions {
+	/// Get the default creation options for a [`MemFd`].
+	///
+	/// Initially, file sealing is not enabled no no huge TLB page size is configured.
+	///
+	/// Note that the close-on-exec flag will always be set on the created file descriptor.
+	/// If you want to pass it to a child process, you should use [`libc::dup2`] or something similar *after forking*.
+	/// Disabling the close-on-exec flag before forking causes a race condition with other threads.
+	pub fn new() -> Self {
+		Self::default()
+	}
+
+	/// Allow sealing operations on the created [`MemFd`].
+	pub fn allow_sealing(&mut self, value: bool) -> &mut Self {
+		self.allow_sealing = value;
+		self
+	}
+
+	/// Create the file in a `hugetlbfs` filesystem using huge pages for the translation look-aside buffer.
+	///
+	/// Support for this feature and specific sizes depend on the CPU and kernel configuration.
+	/// See also: <https://www.kernel.org/doc/html/latest/admin-guide/mm/hugetlbpage.html>
+	pub fn huge_tlb(&mut self, value: Option<HugeTlb>) -> &mut Self {
+		self.huge_table = value;
+		self
+	}
+
+	/// Get the options as raw flags for `libc::memfd_create`.
+	fn as_flags(&self) -> std::os::raw::c_int {
+		let mut flags = sys::flags::MFD_CLOEXEC;
+		if self.allow_sealing {
+			flags |= sys::flags::MFD_ALLOW_SEALING;
+		}
+		#[cfg(target_os = "linux")]
+		if let Some(size) = self.huge_table {
+			flags |= sys::flags::MFD_HUGETLB | size as u32 as std::os::raw::c_int;
+		}
+		flags
+	}
+}
+
+/// Page size for the translation look-aside buffer.
+///
+/// Support for specific sizes depends on the CPU and kernel configuration.
+/// See also: <https://www.kernel.org/doc/html/latest/admin-guide/mm/hugetlbpage.html>
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Ord, PartialOrd, Hash)]
+#[repr(u32)]
+#[non_exhaustive]
+pub enum HugeTlb {
+	Huge64KB = sys::flags::MFD_HUGE_64KB as u32,
+	Huge512KB = sys::flags::MFD_HUGE_512KB as u32,
+	Huge1MB = sys::flags::MFD_HUGE_1MB as u32,
+	Huge2MB = sys::flags::MFD_HUGE_2MB as u32,
+	Huge8MB = sys::flags::MFD_HUGE_8MB as u32,
+	Huge16MB = sys::flags::MFD_HUGE_16MB as u32,
+	Huge32MB = sys::flags::MFD_HUGE_32MB as u32,
+	Huge256MB = sys::flags::MFD_HUGE_256MB as u32,
+	Huge512MB = sys::flags::MFD_HUGE_512MB as u32,
+	Huge1GB = sys::flags::MFD_HUGE_1GB as u32,
+	Huge2GB = sys::flags::MFD_HUGE_2GB as u32,
+	Huge16GB = sys::flags::MFD_HUGE_16GB as u32,
+}
+
+/// Error returned when the file passed to [`MemFd::from_fd`] is not a `memfd`.
+///
+/// This struct contains the [`std::io::Error`] that occurred and the original value passed to `from_fd`.
+/// It is also directly convertible to [`std::io::Error`], so you can pass it up using the `?` operator
+/// from a function that returns an [`std::io::Result`].
+pub struct FromFdError<T> {
+	error: std::io::Error,
+	fd: T,
+}
+
+impl<T> FromFdError<T> {
+	/// Get a reference to the I/O error.
+	pub fn error(&self) -> &std::io::Error {
+		&self.error
+	}
+
+	/// Get a reference to the original file object.
+	pub fn fd(&self) -> &T {
+		&self.fd
+	}
+
+	/// Consume the struct and return the I/O error and the original file object as tuple.
+	pub fn into_parts(self) -> (std::io::Error, T) {
+		(self.error, self.fd)
+	}
+
+	/// Consume the struct and return the I/O error.
+	pub fn into_error(self) -> std::io::Error {
+		self.error
+	}
+
+	/// Consume the struct and return the original file object.
+	pub fn into_fd(self) -> T {
+		self.fd
+	}
+}
+
+impl<T> From<FromFdError<T>> for std::io::Error {
+	fn from(other: FromFdError<T>) -> Self {
+		other.into_error()
+	}
+}
---
+++ src/sys.rs
@@ -0,0 +1,65 @@
+use std::os::raw::c_int;
+use std::os::unix::io::RawFd;
+use filedesc::FileDesc;
+
+#[cfg(any(target_os = "linux", target_os = "freebsd"))]
+mod raw {
+	use std::os::raw::{c_char, c_int};
+	extern "C" {
+		pub fn memfd_create(name: *const c_char, flags: c_int) -> c_int;
+	}
+}
+
+pub fn memfd_create(name: &str, flags: c_int) -> std::io::Result<FileDesc> {
+	let name = std::ffi::CString::new(name)?;
+	let fd = unsafe { raw::memfd_create(name.as_ptr(), flags) };
+	if fd < 0 {
+		Err(std::io::Error::last_os_error())
+	} else {
+		Ok(unsafe { FileDesc::from_raw_fd(fd) })
+	}
+}
+
+pub fn memfd_get_seals(fd: RawFd) -> std::io::Result<c_int> {
+	match unsafe { libc::fcntl(fd, libc::F_GET_SEALS) } {
+		-1 => Err(std::io::Error::last_os_error()),
+		seals => Ok(seals),
+	}
+}
+
+pub fn memfd_add_seals(fd: RawFd, seals: c_int) -> std::io::Result<()> {
+	if unsafe { libc::fcntl(fd, libc::F_ADD_SEALS, seals) } == 0 {
+		Ok(())
+	} else {
+		Err(std::io::Error::last_os_error())
+	}
+}
+
+#[cfg(any(target_os = "linux", target_os = "freebsd"))]
+pub mod flags {
+	// Linux values taken from:
+	// https://github.com/torvalds/linux/blob/1048ba83fb1c00cd24172e23e8263972f6b5d9ac/include/uapi/linux/memfd.h
+	// https://github.com/torvalds/linux/blob/1048ba83fb1c00cd24172e23e8263972f6b5d9ac/include/uapi/asm-generic/hugetlb_encode.h
+	//
+	// FreeBSD values taken from:
+	// https://github.com/freebsd/freebsd-src/blob/de1aa3dab23c06fec962a14da3e7b4755c5880cf/sys/sys/mman.h#L210-L228
+
+	use std::os::raw::c_int;
+	pub const MFD_CLOEXEC: c_int = 0x01;
+	pub const MFD_ALLOW_SEALING: c_int = 0x02;
+	pub const MFD_HUGETLB: c_int = 0x04;
+
+	const MFD_HUGE_SHIFT: c_int = 26;
+	pub const MFD_HUGE_64KB: c_int = 16 << MFD_HUGE_SHIFT;
+	pub const MFD_HUGE_512KB: c_int = 19 << MFD_HUGE_SHIFT;
+	pub const MFD_HUGE_1MB: c_int = 20 << MFD_HUGE_SHIFT;
+	pub const MFD_HUGE_2MB: c_int = 21 << MFD_HUGE_SHIFT;
+	pub const MFD_HUGE_8MB: c_int = 23 << MFD_HUGE_SHIFT;
+	pub const MFD_HUGE_16MB: c_int = 24 << MFD_HUGE_SHIFT;
+	pub const MFD_HUGE_32MB: c_int = 25 << MFD_HUGE_SHIFT;
+	pub const MFD_HUGE_256MB: c_int = 28 << MFD_HUGE_SHIFT;
+	pub const MFD_HUGE_512MB: c_int = 29 << MFD_HUGE_SHIFT;
+	pub const MFD_HUGE_1GB: c_int = 30 << MFD_HUGE_SHIFT;
+	pub const MFD_HUGE_2GB: c_int = 31 << MFD_HUGE_SHIFT;
+	pub const MFD_HUGE_16GB: c_int = 34 << MFD_HUGE_SHIFT;
+}
